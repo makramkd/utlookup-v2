08f224f996ef4fadc1ab36f463fa8434bf46894b
Makram Kamaleddine <makram.kd@gmail.com>
2015-08-25 21:13:55 +0400

    Changed the name of MainActivity to ChooseCampusActivity

--- UTLookup/app/src/main/java/org/lag/utlookup/ChooseCampusActivity.java
+++ UTLookup/app/src/main/java/org/lag/utlookup/ChooseCampusActivity.java
@@ -8,6 +8,6 @@ public class ChooseCampusActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_choose_campus);
     }
 }
