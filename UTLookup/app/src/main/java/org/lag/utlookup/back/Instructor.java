b02949b933741d4f6372692e3ee8c9478bd4747d
Makram Kamaleddine <makram.kd@gmail.com>
2015-08-27 20:41:15 +0400

    Adding the Instructor class. This class is supposed to encapsulate an instructor teaching at a university. We need this java object to easily bind things to the gui when we query the database and get a Cursor object.

---
+++ UTLookup/app/src/main/java/org/lag/utlookup/back/Instructor.java
@@ -0,0 +1,41 @@
+package org.lag.utlookup.back;
+
+import org.lag.utlookup.database.DatabaseContract;
+
+/**
+ * Created by admin on 8/27/15.
+ */
+public class Instructor {
+
+    /**
+     * A unique identification number that is provided by the dbms as a 64 bit
+     * unsigned integer (I think). We keep it here for binding this object to the
+     * gui when the need arises.
+     */
+    public long id;
+
+    /**
+     * The name of the instructor.
+     */
+    public String name;
+
+    /**
+     * The department the instructor belongs to. E.g "CSC".
+     */
+    public String department;
+
+    public Instructor() {
+
+    }
+
+    public Instructor(long id, String name, String department) {
+        this.id = id;
+        this.name = name;
+        this.department = department;
+    }
+
+    public Instructor(String name, String department) {
+        this.name = name;
+        this.department = department;
+    }
+}
