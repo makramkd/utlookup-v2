f1d9d8101bc4237037f4e3f84039bf038d1b2ab4
Makram Kamaleddine <makram.kd@gmail.com>
2015-08-29 11:48:17 +0400

    The first JUNIT test will be on the StGeorgeCrawler, our most important crawler for now.

---
+++ UTLookup/app/src/test/java/org/lag/utlookup/back/StGeorgeCrawlerTest.java
@@ -0,0 +1,60 @@
+package org.lag.utlookup.back;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by admin on 8/29/15.
+ */
+public class StGeorgeCrawlerTest {
+
+    StGeorgeCrawler crawler;
+
+    @Before
+    public void setUp() throws Exception {
+        crawler = new StGeorgeCrawler(StGeorgeCrawler.CALENDAR_URL);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testGetCourseUrls() throws Exception {
+
+    }
+
+    @Test
+    public void testGetDepartmentList() throws Exception {
+
+    }
+
+    @Test
+    public void testGetInstructorList() throws Exception {
+
+    }
+
+    @Test
+    public void testGetCourseList() throws Exception {
+
+    }
+
+    @Test
+    public void testGetDepartmentTimetableLinks() throws Exception {
+
+    }
+
+    @Test
+    public void testPopulateDatabaseFromLink() throws Exception {
+
+    }
+
+    @Test
+    public void testGetCourseDataStore() throws Exception {
+
+    }
+}
