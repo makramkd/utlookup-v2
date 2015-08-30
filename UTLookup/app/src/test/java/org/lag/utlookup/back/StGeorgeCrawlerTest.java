6fa8e86485640da5c93f6a2dce8cf09bce79d476
Makram Kamaleddine <makram.kd@gmail.com>
2015-08-30 11:37:36 +0400

    Commenting out the stdout stuff speeds up the tests by four or more minutes but they’re still way too slow. We’re going to have to improve our networking io by using volley.

--- UTLookup/app/src/test/java/org/lag/utlookup/back/StGeorgeCrawlerTest.java
+++ UTLookup/app/src/test/java/org/lag/utlookup/back/StGeorgeCrawlerTest.java
@@ -30,48 +30,48 @@ public class StGeorgeCrawlerTest {
     @Test
     public void testGetCourseUrls() throws Exception {
         Set<String> courseUrls = crawler.getCourseUrls();
-        System.out.println(STARS);
-        System.out.println("@Test(testGetCourseUrls())");
-        for (String string: courseUrls) {
-            System.out.println(string);
-        }
-        System.out.println(STARS);
+//        System.out.println(STARS);
+//        System.out.println("@Test(testGetCourseUrls())");
+//        for (String string: courseUrls) {
+//            System.out.println(string);
+//        }
+//        System.out.println(STARS);
     }
 
     @Test
     public void testGetDepartmentList() throws Exception {
         List<Department> departmentList = crawler.getDepartmentList();
 
-        System.out.println(STARS);
-        System.out.println("@Test(testGetDepartmentList())");
-        for (Department d : departmentList) {
-            System.out.println("(" + d.code + ", " + d.name + ")");
-        }
-        System.out.println(STARS);
+//        System.out.println(STARS);
+//        System.out.println("@Test(testGetDepartmentList())");
+//        for (Department d : departmentList) {
+//            System.out.println("(" + d.code + ", " + d.name + ")");
+//        }
+//        System.out.println(STARS);
     }
 
     @Test
     public void testGetInstructorList() throws Exception {
         List<Instructor> instructorList = crawler.getInstructorList();
 
-        System.out.println(STARS);
-        System.out.println("@Test(testGetInstructorList())");
-        for (Instructor i : instructorList) {
-            System.out.println("(" + i.name + "," + i.department + ")");
-        }
-        System.out.println(STARS);
+//        System.out.println(STARS);
+//        System.out.println("@Test(testGetInstructorList())");
+//        for (Instructor i : instructorList) {
+//            System.out.println("(" + i.name + "," + i.department + ")");
+//        }
+//        System.out.println(STARS);
     }
 
     @Test
     public void testGetCourseList() throws Exception {
         List<Course> courses = crawler.getCourseList();
 
-        System.out.println(STARS);
-        System.out.println("@Test(testGetCourseList())");
-        for (Course c : courses) {
-            System.out.println("(" + c.courseCode + "," + c.courseName + ")");
-        }
-        System.out.println(STARS);
+//        System.out.println(STARS);
+//        System.out.println("@Test(testGetCourseList())");
+//        for (Course c : courses) {
+//            System.out.println("(" + c.courseCode + "," + c.courseName + ")");
+//        }
+//        System.out.println(STARS);
     }
 
     @Test
@@ -79,12 +79,12 @@ public class StGeorgeCrawlerTest {
         crawler.setUrl(StGeorgeCrawler.TIMETABLE_URL_FW);
         List<String> departmentTimetableLinks = crawler.getDepartmentTimetableLinks();
 
-        System.out.println(STARS);
-        System.out.println("@Test(testGetDepartmentTimetableLinks())");
-        for (String departmentLink : departmentTimetableLinks) {
-            System.out.println(departmentLink);
-        }
-        System.out.println(STARS);
+//        System.out.println(STARS);
+//        System.out.println("@Test(testGetDepartmentTimetableLinks())");
+//        for (String departmentLink : departmentTimetableLinks) {
+//            System.out.println(departmentLink);
+//        }
+//        System.out.println(STARS);
     }
 
     @Test
@@ -101,12 +101,12 @@ public class StGeorgeCrawlerTest {
     public void testGetDepartmentOfferings() throws Exception {
         List<DepartmentOffering> departmentOfferings = crawler.getDepartmentOfferings();
 
-        System.out.println(STARS);
-        System.out.println("@Test(testGetDepartmentOfferings())");
-        for (DepartmentOffering departmentOffering : departmentOfferings) {
-            System.out.println("(" + departmentOffering.deptCode + ", " +
-            departmentOffering.courseCode + ")");
-        }
-        System.out.println(STARS);
+//        System.out.println(STARS);
+//        System.out.println("@Test(testGetDepartmentOfferings())");
+//        for (DepartmentOffering departmentOffering : departmentOfferings) {
+//            System.out.println("(" + departmentOffering.deptCode + ", " +
+//            departmentOffering.courseCode + ")");
+//        }
+//        System.out.println(STARS);
     }
 }
