524e060e55fa86b9546c774f896377a817e150f4
Makram Kamaleddine <makram.kd@gmail.com>
2015-08-30 13:36:30 +0400

    Some testing code to run the request queuing. It’s definitely faster than the synchronous JSoup connect with timeout function.

--- UTLookup/app/src/main/java/org/lag/utlookup/ChooseCampusActivity.java
+++ UTLookup/app/src/main/java/org/lag/utlookup/ChooseCampusActivity.java
@@ -2,12 +2,55 @@ package org.lag.utlookup;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.android.volley.toolbox.StringRequest;
+
+import org.lag.utlookup.back.AsyncStGeorgeCrawler;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ChooseCampusActivity extends Activity {
+
+    AsyncStGeorgeCrawler crawler;
+    List<StringRequest> instructorStringRequests;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_campus);
+
+        crawler = new AsyncStGeorgeCrawler();
+        instructorStringRequests = new ArrayList<>();
+    }
+
+    public void getInstructorRequestsButtonClicked(View view) {
+        // can only work when we get course urls
+        if (crawler.courseUrls.isEmpty()) {
+            return;
+        }
+
+        for (String courseUrl : crawler.courseUrls) {
+            instructorStringRequests.add(
+                    crawler.getInstructorListForDepartmentStringRequest(courseUrl)
+            );
+        }
+    }
+
+    public void getCoursesButtonClicked(View view) {
+        StringRequest courseRequest = crawler.getCourseUrlsStringRequest();
+        ApplicationController.getInstance().queueRequest(courseRequest);
+    }
+
+    public void executeInstructorRequestsButtonClicked(View view) {
+        // can only work if we have some requests
+        if (instructorStringRequests.isEmpty()) {
+            return;
+        }
+
+        for (StringRequest request : instructorStringRequests) {
+            ApplicationController.getInstance().queueRequest(request);
+        }
     }
 }
